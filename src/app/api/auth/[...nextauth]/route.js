38132761f187feef7ff03d72c52cf708b50c5d1d
coldeyes <coldeyesce@gmail.com>
2026-03-30 22:03:12 +0800

    Revert "Harden auth flow for CSRF on Cloudflare"
    This reverts commit 5b77f6ea0a06f88c137cf8e6c72444786bb62523.

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,3 @@
 import { handlers } from "@/auth"
 export const runtime = 'edge';
-export const dynamic = 'force-dynamic';
-export const fetchCache = 'force-no-store';
 export const { GET, POST } = handlers
